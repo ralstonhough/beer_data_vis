d6414877d304af9e0bcf6f0c1789e3e88f3cdb80
Ralston Hough <ralstonhough@gmail.com>
2022-11-15 13:47:08 -0500

    Glasses and parameter corrections

--- script.js
+++ script.js
@@ -25,19 +25,45 @@ fetch(opensheet_uri)
             dataArr.push([name, abv, ibu, srm, rating, type, season]);
 
             let fluidBox = document.createElement("DIV");
+            
             fluidBox.classList.add("fluid");
+            if (type == "Lager"){
+                fluidBox.classList.add("lager");
+            };
+            if (type == "IPA"){
+                fluidBox.classList.add("ipa");
+            };
+            if (type == "Witbier"){
+                fluidBox.classList.add("witbier");
+            };
+            if (type == "Belgian"){
+                fluidBox.classList.add("belgian");
+            };
+            if (type == "Spiced Ale"){
+                fluidBox.classList.add("spicedAle");
+            };
+            if (type == "Shandy"){
+                fluidBox.classList.add("shandy");
+            };
+            if (type == "Stout"){
+                fluidBox.classList.add("stout");
+            };
+            if (type == "Porter"){
+                fluidBox.classList.add("porter");
+            };
+            
             document.getElementById("grid").appendChild(fluidBox);
 
             let lightness = map(srm,1,50,60,0);
-            fluidBox.style.background = `hsla(46, 100%, ${lightness}%, 1)`;
+            fluidBox.style.background = `linear-gradient(0deg, hsl(46, 100%, ${lightness}%) 75%, rgba(255,255,255,1) 100%)`;
             
             let fullness = map(rating,3,4,1,20);
             fluidBox.style.height = `${fullness}%`;
             
-            let hops = map(ibu,0,60,2000,100);
+            let hops = map(abv,4,10,2000,100);
             fluidBox.style.animation = `hop ${hops}ms infinite`;
 
-            let bubbles = map(abv,4,10,1,100);
+            let bubbles = map(ibu,0,60,1,100);
             for (let i=0; i<bubbles; i++){
                 let bubble = document.createElement("DIV");
                 bubble.classList.add("bubble");
@@ -48,7 +74,7 @@ fetch(opensheet_uri)
                 let randomTime = Math.random() * (500 - 100) + 100;
                 bubble.style.width = randomSize + "%";
                 bubble.style.height = randomSize + "%";
-                bubble.style.background = `hsla(46, 100%, ${lightness + 20}%, 1)`;
+                bubble.style.background = `hsla(46, 100%, ${lightness + 15}%, .5)`;
                 bubble.style.left = randomLeft + "%";
                 bubble.style.top = randomTop + "%";
                 bubble.style.animation = `fizz ${randomTime}ms infinite`;
